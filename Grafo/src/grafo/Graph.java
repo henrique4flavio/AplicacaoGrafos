24fd819ef712490128933d75bfa9b5a846d12860
LaisAlves <laisalvesjf@hotmail.com>
2017-10-05 19:11:15 -0300

    Criação da Classe Graph

---
+++ Grafo/src/grafo/Graph.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafo;
+
+import java.util.ArrayList;
+import javafx.scene.Node;
+
+public class Graph {
+    private ArrayList<Node> node  = new ArrayList<>();
+private ArrayList<Aresta> edged = new ArrayList<>();
+public ArrayList<Node> getNode() {
+        return node;
+    }
+
+    public void setNode(ArrayList<Node> node) {
+        this.node = node;
+    }
+
+    public ArrayList<Aresta> getEdged() {
+        return edged;
+    }
+
+    public void setEdged(ArrayList<Aresta> edged) {
+        this.edged = edged;
+    }
+
+}
