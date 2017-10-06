9f612cac00b98c7afcab4e1a7f0ab869a30596e6
Flávio Henrique de Almeida Junior <flaviohenriquejf@gmail.com>
2017-10-06 14:24:04 -0300

    Inserção de clase
    Inserção da classe (Aresta.java)

---
+++ Grafo/src/grafo/Aresta.java
@@ -0,0 +1,26 @@
+package grafo;
+
+import java.util.ArrayList;
+
+public class Aresta {
+
+    private String source;
+    private String target;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+}
