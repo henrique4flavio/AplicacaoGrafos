df29be8535b255bfcfd7b2e39093c455cd193df5
Jonathas Araújo <jonathas.araujo@engenharia.ufjf.br>
2017-10-06 23:27:32 -0300

    Criação da Classe Node
    Criação da classe Node com seus métodos e atributos

---
+++ Grafo/src/grafo/Node.java
@@ -0,0 +1,19 @@
+package grafo;
+
+public class Node {
+
+    private String id;
+
+    public Node(String text) {
+        this.id = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
