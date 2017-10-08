59b9f6805f18b28260d8f27e00ce928e7bd36db1
Flávio Henrique de Almeida Junior <flaviohenriquejf@gmail.com>
2017-10-08 18:31:39 -0300

    Inserção de botão na interface

--- Grafo/src/grafo/Graph.java
+++ Grafo/src/grafo/Graph.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grafo;
 
 import java.util.ArrayList;
 import javafx.scene.Node;
 
 public class Graph {
-    private ArrayList<Node> node  = new ArrayList<>();
-private ArrayList<Aresta> edged = new ArrayList<>();
-public ArrayList<Node> getNode() {
+
+    private ArrayList<Node> node = new ArrayList<>();
+    private ArrayList<Aresta> edged = new ArrayList<>();
+
+    public ArrayList<Node> getNode() {
         return node;
     }
 
